11d9d68456e73f37db1f6c63ac9b96416293a928
Maksim Lin <maksim.lin@ngv.vic.gov.au>
2010-07-05 13:24:26 +1000

    initial draft of fork to implement client to work in sync manner with ringojs.

--- redis.js
+++ redis.js
@@ -1,5 +1,4 @@
-// redis.js - a Redis client for server-side JavaScript, in particular Node
-// which runs atop Google V8.
+// redis.js - a Redis client for RingoJS server-side JavaScript
 //
 // Please review the Redis command reference and protocol specification:
 //
@@ -15,20 +14,12 @@
 // adds a completely new response type (other than status code, integer, error,
 // bulk, and multi-bulk).  See http://code.google.com/p/redis/wiki/ReplyTypes
 //
-// To learn more about Node and Google V8, see http://tinyclouds.org/node/ and
-// http://code.google.com/p/v8/ respectively.
+// To learn more about RingoJS, see http://ringojs.org.
 //
-// Brian Hammond, Fictorial, June 2009
+// Maksim Lin <maksim.lin@ngv.vic.gov.au>
 
-/**
- * UPDATED - updated Brian's code to use the node API as of
- * node version 0.1.7
- *
- * Also, changed the way data is received to account for partial
- * chunks in multi-bulk responses.
- *
- * Brit Gardner, http://britg.com, Aug 2009
- **/
+
+export('connect', 'debugMode');
 
 // debugMode:
 // We don't use print() or puts() immediately as they are asynchronous in Node;
@@ -37,16 +28,20 @@
 // error via throw in debugMode, we dump all output, *then* throw.  This is
 // useful for, well, debugging.  Otherwise, turn off debugMode (which is the
 // default).
-this.debugMode = false;
+var debugMode = true;
 
 function debug(data) {
   if(exports.debugMode) {
-    node.debug(data.replace(/\r\n/g, '\\r\\n'));
+      var ringo = require('ringo/shell');
+      ringo.writeln(data);
   }
 }
 
+var {PrintWriter, BufferedReader, InputStreamReader} = Packages.java.io;
+
 var conn, _host, _port;
 var lastPrefix, lastHandler, lastChunk;
+var _out, _inp;
 
 var chunks = [];
 var callbacks = [];
@@ -54,10 +49,12 @@ var callbacks = [];
 var CRLF = "\r\n";
 var CRLF_LENGTH = 2;
 
+
+
 /**
  * Public interface to create a connection
  **/
-this.connect = function(port, host) {
+function connect(port, host) {
   _port = port || 6379;
   _host = host || '127.0.0.1';
   _connect();
@@ -83,21 +80,12 @@ function _connect(cb, args) {
   lastPrefix = lastHandler = lastChunk = null;
 
   // create connection and set encoding to UTF8
-  conn = node.tcp.createConnection(_port, _host);
-  conn.setEncoding("utf8");
+  conn = new Packages.java.net.Socket('localhost', 6379);
 
-  // On connect event, format command
-  // and send
-  conn.addListener("connect", function() {
-    if(typeof cb == 'function') {
-      cb.apply(exports);
-    }
-  });
+  _out = new PrintWriter(conn.getOutputStream(), true);
+  _inp =  new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
-  // On receive data event
-  conn.addListener('receive', function(data) {
-    processChunk(data);
-  });
+
 }
 
 /**
@@ -117,7 +105,7 @@ function processChunk(chunk) {
   if (chunk.length == 0) {
     fatal("empty response");
   }
-    
+
   // Is this beginning of a Redis response?
   var startPrefix   = chunk.charAt(0);
   var startHandler  = replyPrefixToHandler[startPrefix];
@@ -165,7 +153,7 @@ function processResponseStart(prefix, handler, chunk) {
 
 
 /**
- * Do some tidying up, and apply callbacks 
+ * Do some tidying up, and apply callbacks
  **/
 function processResponseEnd(data) {
 
@@ -248,7 +236,7 @@ function fatal(errorMessage) {
 }
 
 function maybeConvertToNumber(str) {
-  if (/^\s*\d+\s*$/.test(str)) 
+  if (/^\s*\d+\s*$/.test(str))
     return parseInt(str, 10);
 
   if (/^\s*\d+\.(\d+)?\s*$/.test(str))
@@ -278,15 +266,15 @@ function formatBulk(commandName, commandArgs, argCount) {
   var args = commandName;
 
   for (var i = 0; i < argCount - 1; ++i) {
-    var val = typeof(commandArgs[i]) != 'string' 
-      ? commandArgs[i].toString() 
+    var val = typeof(commandArgs[i]) != 'string'
+      ? commandArgs[i].toString()
       : commandArgs[i];
 
     args += ' ' + val;
   }
 
-  var lastArg = typeof(commandArgs[argCount - 1]) != 'string' 
-    ? commandArgs[argCount - 1].toString() 
+  var lastArg = typeof(commandArgs[argCount - 1]) != 'string'
+    ? commandArgs[argCount - 1].toString()
     : commandArgs[argCount - 1];
 
   var cmd = args + ' ' + lastArg.length + CRLF + lastArg + CRLF;
@@ -301,44 +289,29 @@ function createCommandSender(commandName) {
 
     var commandArgs = arguments;
 
-    if (conn.readyState != "open") {
-      debug('Connection is not open (' + conn.readyState + ')');
-      conn.close();
-      _connect(function() {
-        exports[commandName].apply(exports, commandArgs);
-      });
-      return;
-    }
-
-    // last arg (if any) should be callback function.
-
-    var callback = null;
-    var numArgs = commandArgs.length;
-
-    if (typeof(commandArgs[commandArgs.length - 1]) == 'function') {
-      callback = commandArgs[commandArgs.length - 1];
-      numArgs = commandArgs.length - 1;
+    if (!conn.isConnected()) {
+      debug('Connection is not open - attempting to reopen');
+      _connect();
     }
 
     // Format the command and send it.
-
     var cmd;
 
     if (inlineCommands[commandName]) {
-      cmd = formatInline(commandName, commandArgs, numArgs);
+      cmd = formatInline(commandName, commandArgs, commandArgs.length);
     } else if (bulkCommands[commandName]) {
-      cmd = formatBulk(commandName, commandArgs, numArgs);
-    } else { 
+      cmd = formatBulk(commandName, commandArgs, commandArgs.length);
+    } else {
       fatal('unknown command ' + commandName);
     }
-      
+
     debug('> ' + cmd);
 
     // Always push something, even if its null.
     // We need received replies to match number of entries in `callbacks`.
 
-    callbacks.push({ cb:callback, cmd:commandName.toLowerCase() });
-    conn.send(cmd);
+    //dont try to send anything yet...!
+    //conn.send(cmd);
   };
 }
 
@@ -359,12 +332,12 @@ function handleBulkReply(reply, offset) {
   var crlfIndex = reply.indexOf(CRLF, offset);
   var valueLength = parseInt(reply.substr(offset, crlfIndex - offset), 10);
 
-  if (valueLength == -1) 
+  if (valueLength == -1)
     return [ null, crlfIndex + CRLF_LENGTH ];
 
   var value = reply.substr(crlfIndex + CRLF_LENGTH, valueLength);
 
-  var nextOffset = crlfIndex   + CRLF_LENGTH + 
+  var nextOffset = crlfIndex   + CRLF_LENGTH +
                    valueLength + CRLF_LENGTH;
 
   return [ value, nextOffset ];
@@ -378,7 +351,7 @@ function handleMultiBulkReply(reply, offset) {
 
   offset = crlfIndex + CRLF_LENGTH;
 
-  if (count === -1) 
+  if (count === -1)
     return [ null, offset ];
 
   var entries = [];
@@ -398,9 +371,9 @@ function handleSingleLineReply(reply, offset) {
   var crlfIndex = reply.indexOf(CRLF, offset);
   var value = reply.substr(offset, crlfIndex - offset);
 
-  // Most single-line replies are '+OK' so convert such to a true value. 
+  // Most single-line replies are '+OK' so convert such to a true value.
 
-  if (value === 'OK') 
+  if (value === 'OK')
     value = true;
 
   return [ value, crlfIndex + CRLF_LENGTH ];
@@ -411,7 +384,7 @@ function handleIntegerReply(reply, offset) {
 
   var crlfIndex = reply.indexOf(CRLF, offset);
 
-  return [ parseInt(reply.substr(offset, crlfIndex - offset), 10), 
+  return [ parseInt(reply.substr(offset, crlfIndex - offset), 10),
            crlfIndex + CRLF_LENGTH ];
 }
 
@@ -479,8 +452,10 @@ function postProcessResults(command, result) {
 //   'lexicographically': true|false
 
 exports.sort = function(key, options, callback) {
-  if (conn.readyState != "open")
-    fatal("connection is not open");
+  if (!conn.isConnected()) {
+      fatal("connection is not open");
+  }
+
 
   var cmd = 'sort ' + key;
 
@@ -497,20 +472,20 @@ exports.sort = function(key, options, callback) {
     var optAsc   = options.ascending         ? ''      : 'desc';
     var optAlpha = options.lexicographically ? 'alpha' : '';
 
-    var optLimit = options.limit 
-      ? 'limit ' + options.limit[0] + ' ' + options.limit[1] 
+    var optLimit = options.limit
+      ? 'limit ' + options.limit[0] + ' ' + options.limit[1]
       : '';
 
     cmd += ' ' + optBy    + ' ' +
                  optLimit + ' ' +
                  optGet   + ' ' +
-                 optAsc   + ' ' + 
+                 optAsc   + ' ' +
                  optAlpha + ' ' + CRLF;
 
     cmd = cmd.replace(/\s+$/, '') + CRLF;
   }
-  
-  if (exports.debugMode) 
+
+  if (exports.debugMode)
     debug('> ' + cmd);
 
   conn.send(cmd);
@@ -524,8 +499,9 @@ exports.sort = function(key, options, callback) {
 // Close the connection.
 
 exports.quit = function() {
-  if (conn.readyState != "open")
-    fatal("connection is not open");
+  if (!conn.isConnected()) {
+      fatal("connection is not open");
+  }
 
   debug('> quit');
 
