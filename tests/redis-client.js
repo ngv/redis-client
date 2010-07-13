eb74acae31394d5d47812ece29982087cd7ddb72
Maksim Lin <maks@manichord.com>
2010-07-14 09:47:49 +1000

    initial unit tests using ringo's testing framework.

---
+++ tests/redis-client.js
@@ -0,0 +1,49 @@
+// Test suite for node (ECMAScript) redis client.
+//
+// NOTE: you must have started a redis-server instance on (the default)
+// 127.0.0.1:6379 prior to running this test suite.
+//
+// NOTE: this test suite uses databases 14 and 15 for test purposes! It will
+// **clear** this database at the start of the test runs.  If you want to use
+// a different database number, update TEST_DB_NUMBER* below.
+//
+
+var TEST_DB_NUMBER = 15;
+var TEST_DB_NUMBER_FOR_MOVE = 14;
+
+var assert = require('assert');
+
+var redis = require('ngv/redis');
+
+exports.setUp = function () {
+	 
+	assert.isTrue(redis.connect());
+	
+	assert.isTrue(redis.select(TEST_DB_NUMBER_FOR_MOVE));
+	assert.strictEqual(redis.dbsize(), 0);
+	 
+	assert.isTrue(redis.select(TEST_DB_NUMBER));
+	assert.strictEqual(redis.dbsize(), 0);
+	 
+};
+
+exports.tearDown = function () {	
+	assert.isTrue(redis.flushdb()); 
+	assert.strictEqual(redis.dbsize(), 0);
+};
+
+exports.testSet = function() {
+	assert.isTrue(redis.set('foo', 'bar'));
+	assert.isTrue(redis.set('baz', 'buz'));
+}
+
+exports.testSetNX = function() {
+	assert.strictEqual(redis.setnx('foo', 'quux'), 0);  // fails when already set
+	assert.strictEqual(redis.setnx('boo', 'apple'), 1 );  // no such key already so OK
+}
+
+exports.testGet = 	function() {
+	assert.strictEqual(redis.get('foo'), 'bar');
+	assert.strictEqual(redis.get('boo'), 'apple');
+}
+
